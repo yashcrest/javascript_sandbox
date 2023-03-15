2a6bd3d64b72da781bfa36a39357435c9ef3b5dd
Yash Shrestha <yashcrest@gmail.com>
2023-03-15 18:25:18 +1030

    second commit

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -1,3 +1,31 @@
-//this is test file and can be ignored
+const items = ['array' , 'damn this things' , 'what the hell?']
 
-function test ()
+//console.log(Array.isArray(items)); // checks if the item is array or not.
+
+
+const todos = [
+    {
+        id: 1,
+        text: 'take out bin',
+        iscompleted: false
+    },
+    {
+        id:2,
+        text: "Don't take bin out",
+        iscompleted: true
+    },
+    {
+        id:3,
+        text: "What is up my brother",
+        iscompleted: false
+    }
+];
+
+//console.log(todos[0].text); //this will print text inside the first element in array
+
+const todoJSON = JSON.stringify(todos);
+
+//looping for loop
+todos.forEach(function(todos){
+    console.log(todos.text);
+});
