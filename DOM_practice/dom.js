904cd7320950eda1e0079cc9b140085ffbb8d885
yashcrest <yashcrest@gmail.com>
2023-03-30 23:20:02 +1030

    message

--- DOM_practice/dom.js
+++ DOM_practice/dom.js
@@ -1,1 +1,29 @@
-console.dir(document)
+// console.dir(document);
+// console.log(document.all);
+// document.title = "Yash's Sandbox"
+// console.log(document.all[10]);
+// document.all[10].innerHTML = 'changed';
+// document.all[13].textContent = 'changed again';
+// console.log(document.forms)
+// console.log(docuemnt.links)
+
+// console.log(document.getElementById("header-title"));
+// console.log(document.getElementById('header-title'))
+
+let items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[0]);
+
+// items[1].textContent = 'Hello';
+// items[1].style.fontWeight = 'bold';
+// items[2].style.backgroundColor  = 'yellow'; 
+
+// items.style.backgroundColor = 'yellow';
+
+for(let i =0; i< items.length; i++){
+    items[i].style.backgroundColor = 'yellow';
+}
+
+items.forEach((element,index) => {
+    element[index].style.backgroundColor = 'yellow'
+});
