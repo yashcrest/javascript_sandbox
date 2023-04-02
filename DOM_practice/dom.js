cc888e21885db1b4094e003cd8592794b028c3ed
yashcrest <yashcrest@gmail.com>
2023-04-02 17:55:28 +0930

    message

--- DOM_practice/dom.js
+++ DOM_practice/dom.js
@@ -10,7 +10,7 @@
 // console.log(document.getElementById("header-title"));
 // console.log(document.getElementById('header-title'))
 
-let items = document.getElementsByClassName('list-group-item');
+// let items = document.getElementsByClassName('list-group-item');
 // console.log(items);
 // console.log(items[0]);
 
@@ -20,10 +20,79 @@ let items = document.getElementsByClassName('list-group-item');
 
 // items.style.backgroundColor = 'yellow';
 
-for(let i =0; i< items.length; i++){
-    items[i].style.backgroundColor = 'yellow';
-}
+// for(let i =0; i< items.length; i++){
+//     items[i].style.backgroundColor = 'yellow';
+// }
 
-items.forEach((element,index) => {
-    element[index].style.backgroundColor = 'yellow'
+//queryselector
+// let tests = document.querySelectorAll('li');
+// for (i =0; i<tests.length; i++){
+//     tests[i].style.borderBottom = 'solid 4px grey';
+// }
+
+// let input = document.querySelector('input');
+// input.placeholder = 'Hello world';
+// console.log(input);
+
+// let submit = document.querySelector('input[type="submit"]');
+// submit.value = 'SEND';
+
+// //QuerySelectorAll 
+// let odd = document.querySelectorAll('li:nth-child(odd)');
+// let even = document.querySelectorAll('li:nth-child(even)');
+// odd.forEach(item => item.style.backgroundColor = '#f4f4f4');
+// even.forEach(item => item.style.backgroundColor = '#ccc');
+
+
+//Traversing the DOM
+let itemList = document.querySelector('#Items');
+let form = document.querySelector('form');
+//parentNode 
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = "#f4f4f4";
+// console.log(itemList.parentNode.parentNode); 
+
+//parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = "#f4f4f4";
+// console.log(itemList.parentElement.parentElement);
+
+//childNodes
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+
+//fistChild
+// console.log(itemList.lastElementChild);
+
+//nextSiblig
+// console.log(form.nextSibling);
+//nextElementSibling
+// console.log(itemList.nextSibling);  
+//previousSibling
+// console.log(itemList.previousSibling);
+// console.log(itemList.previousElementSibling);
+
+
+//CreateElement
+let newDiv = document.createElement('div');
+newDiv.className ='created-via-JS';
+newDiv.id = 'myDiv'
+console.log(newDiv);
+
+//creating a text node
+let newDivtext = document.createTextNode('This is created via JS');
+newDiv.appendChild(newDivtext);
+
+//adding the element to DOM
+let container = document.querySelector('header .container ');
+let h1 = document.querySelector('header h1');
+container.insertBefore(newDiv, h1);
+
+newDiv.style.fontSize = '30px';
+
+//Event Listner
+let btn = document.querySelector('#button');
+btn.addEventListener('click',()=> {
+    document.getElementById('myDiv').textContent = 'changed';
+    document.querySelector('#main').style.backgroundColor = '#f4f4f4';
 });
