dd98f6e7d712d215ad690e1ee5f347809708b8f4
Yash Shrestha <yashcrest@gmail.com>
2023-05-25 16:26:13 +0930

    commit

--- Async_JS/callbacks.js
+++ Async_JS/callbacks.js
@@ -1,6 +1,4 @@
 fetch('https://api.weatherapi.com/v1/current.json?q=kathmandu&lang=eu&key=34d6d8dc8a584ef6b4d92016232105')
 .then(response => response.json())
-.then(data = document.body.innerHTML = JSON.stringify(data));
-
-
+.then(data => document.body.innerHTML = JSON.stringify(data));
 
