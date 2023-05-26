fa45d317dc1a8ef5f007d6a58b38c72cc3013513
Yash Shrestha <yashcrest@gmail.com>
2023-05-26 21:38:55 +0930

    commiting

--- Async_JS/promises.js
+++ Async_JS/promises.js
@@ -106,31 +106,30 @@ function makeRequest(location) {
 
 function processRequest(response){
     return new Promise((resolve, reject)=> {
-        console.log('Processing response')
-        resolve(`Extra Information + ${response}`)
+        console.log('Processing response');
+        resolve(`Extra Information + ${response}`);
     })
 }
 
 makeRequest('Google').then(response => {
-    console.log('Response received')
-    return processRequest(response)
+    console.log('Response received');
+    return processRequest(response);
 }).then(processedResponse => {
-    console.log(processedResponse)
+    console.log(processedResponse);
 }).catch(err => {
-    console.log(err)
+    console.log(err);
 })
 
 
 //Now using Async/Await
 async function doWork(){
     try {const response =  await makeRequest('Facebook')
-    console.log('response received')
-    const processedResponse = await processRequest(response)
+    console.log('response received');
+    const processedResponse = await processRequest(response);
     console.log(processedResponse);
     } catch(error) {
         console.log(error);
     }
    
 }
-
-doWork();
+doWork(); 
